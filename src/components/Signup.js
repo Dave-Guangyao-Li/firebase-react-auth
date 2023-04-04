e47f481fb7900653da086428846241962bc433b6
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-04-04 00:19:17 -0700

    signup functionality implement

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,44 @@
 // signup component, renders a signup form, and a button to submit the form
-import React, { useRef } from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+import { setSelectionRange } from '@testing-library/user-event/dist/utils'
+import React, { useRef, useState } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext' // this is the custom hook that is used to access the context object
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const { signup, currentUser } = useAuth() // this is the signup function that is passed to the context object
+    const [error, setError] = useState('') // this is the error message that is displayed if there is an error
+    const [loading, setLoading] = useState(false) // this is used to disable the button when the form is submitted
+
+
+    async function handleSubmit(e) { // this is the function that is called when the form is submitted
+        e.preventDefault()
+
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) { // if the password and the password confirmation do not match, return
+            return setError('Passwords do not match')
+        }
+
+        try {
+            setError('') // if there is an error, set the error message to an empty string
+            setLoading(true) // set loading to true, this will disable the button
+            await signup(emailRef.current.value, passwordRef.current.value) // await is used to wait for the promise to resolve, if the promise is rejected, the error will be caught in the catch block
+        } catch {
+            setError('Failed to create an account')
+        }
+        setLoading(false) // set loading to false, this will enable the button
+
+    }
 
     return (
         <>
             <Card>
                 <Card.Body>
                     <h2 className="text-center mb-4">Sign Up</h2>
-                    <Form>
+                    {currentUser && currentUser.email}
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group id="email" className="mb-4">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" ref={emailRef} required />
@@ -25,7 +51,7 @@ export default function Signup() {
                             <Form.Label>Password Confirmation</Form.Label>
                             <Form.Control type="password" ref={passwordConfirmRef} required />
                         </Form.Group>
-                        <Button className="w-100" type="submit">Sign Up</Button>
+                        <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
                     </Form>
                 </Card.Body>
             </Card>
