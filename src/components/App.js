3a50d0742faff722d29230ad7ae0b41821204a08
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-04-07 11:29:00 -0700

    set up react router

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../contexts/AuthContext';
 import Signup from './Signup';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: "100vh" }}>
       <div className='w-100' style={{ maxWidth: "400px" }} >
-        <AuthProvider>
-          <Signup />
-        </AuthProvider>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              {/* <Route path="/forgot-password" component={ForgotPassword} /> */}
+            </Switch>
+            <Signup />
+          </AuthProvider>
+        </Router>
+
       </div>
 
     </Container >
